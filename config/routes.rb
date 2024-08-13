d2c621488f5e085375e7f33b698af412ed5f0739
EC2 Default User <ec2-user@ip-172-31-34-19.ap-northeast-3.compute.internal>
2024-08-13 13:31:12 +0000

    edit完了

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :books
   get '/' => 'homes#top'
   get '/books/:id' => 'books#show'
+  get '/books/:id/edit' => 'books#edit'
+  patch '/books/:id' => 'books#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
